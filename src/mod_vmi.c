000cf286747acaea28d2e067b4f379cccd0a804a
ssjtoma <ssjtoma@c0522127-aac4-4857-90a6-cf3fa1cde04f>
2012-09-27 14:58:19 +0000

    VMI module update.
    git-svn-id: svn://svn.code.sf.net/p/honeybrid/code@40 c0522127-aac4-4857-90a6-cf3fa1cde04f

--- src/mod_vmi.c
+++ src/mod_vmi.c
@@ -36,37 +36,50 @@
 #include "modules.h"
 #include "netcode.h"
 
+#define ATTACK_TIMEOUT 600
+
 GTree *vmi_vms;
+GTree *bannedIPs;
 int vmi_sock;
 
 struct sockaddr_in vmi_addr;    /* VMI server address */
 unsigned short vmi_port;    /* VMI server port */
 
-struct vmi_msg {
+/*struct vmi_msg {
 	char *vm_name;
 	char *attacker;
 	char *conn_out;
-};
+};*/
 
 struct vm_search {
 	gchar *srcIP;
 	uint32_t vmID;
 };
 
+struct attacker_search {
+	gchar *outIP;
+	gchar *attacker;
+	uint32_t mark;
+
+	char *vmName;
+	gint found;
+	gint event;
+};
+
 struct vmi_vm {
 	gchar *attacker;
 	char *name;
 	uint32_t vmID;
-	uint32_t start;
-	uint32_t last_seen;
 	uint32_t logID;
 	int socket;
 	int paused;
+	gint start;
+	gint event;
 
 	pthread_mutex_t lock;
 };
 
-void free_vmi_msg(gpointer data) {
+/*void free_vmi_msg(gpointer data) {
         struct vmi_msg *tofree = (struct vmi_msg *)data;
 	if(tofree->vm_name!=NULL)
 		free(tofree->vm_name);
@@ -75,7 +88,7 @@ void free_vmi_msg(gpointer data) {
 	if(tofree->conn_out!=NULL)
 		free(tofree->conn_out);
         free(tofree);
-}
+}*/
 
 void noop(gpointer data) {}
 
@@ -135,9 +148,8 @@ gboolean build_vmi_vms2(gpointer key, gpointer value, gpointer data) {
 				vm->vmID=*(uint32_t*)key;
 				vm->name=strdup(vm_name);
 				vm->attacker=NULL;
-				vm->start=0;
-				vm->last_seen=0;
 				vm->paused=1;
+				vm->event=0;
 
 				pthread_mutex_init(&(vm->lock),NULL);
 
@@ -151,10 +163,9 @@ gboolean build_vmi_vms2(gpointer key, gpointer value, gpointer data) {
                                 vm->vmID=*(uint32_t*)key;
                                 vm->name=strdup(vm_name);
                                 vm->attacker=NULL;
-                                vm->start=0;
-                                vm->last_seen=0;
 				vm->paused=1;
 				vm->logID=0;
+				vm->event=0;
 
                                 pthread_mutex_init(&(vm->lock),NULL);
 
@@ -187,21 +198,6 @@ gboolean find_free_vm(gpointer key, gpointer value, gpointer data) {
 
 	pthread_mutex_lock(&(vm->lock));
 
-	// the VM could have been revert by a scheduled scan, check status
-	if(!vm->paused) {
-		char buf[100];
-		bzero(buf,100);
-		sprintf(buf,"status,%s\n",vm->name);
-		write(vmi_sock, buf, strlen(buf));
-		bzero(buf,100);
-		read(vmi_sock,buf,100);
-		if(!strcmp(buf,"paused\n\r")) {
-			vm->paused=1;
-			free(vm->attacker);
-			vm->attacker=NULL;
-		}
-	}
-
 	if(vm->paused) {
 
 		char buf[100];
@@ -223,6 +219,11 @@ gboolean find_free_vm(gpointer key, gpointer value, gpointer data) {
 		vm->logID=atoi(strtok_r(NULL,delim,&p));
 		vm->paused=0;
 		vm->attacker=g_strdup(search->srcIP);
+		GTimeVal t;
+	        g_get_current_time(&t);
+        	vm->start = (t.tv_sec);
+		vm->event=0;
+
 		search->vmID=*vmID;
 		found=1;
 
@@ -243,9 +244,22 @@ gboolean find_used_vm(gpointer key, gpointer value, gpointer data) {
         struct vm_search *search=(struct vm_search *)data;
 
 	if(!vm->paused && !strcmp(search->srcIP, vm->attacker)) {
-		printf("This attacker is already using a VM: %u\n", *vmID);
-		search->vmID=*vmID;
-		return TRUE;
+
+		char buf[100];
+		bzero(buf,100);
+		sprintf(buf,"status,%s\n",vm->name);
+		write(vmi_sock, buf, strlen(buf));
+		bzero(buf,100);
+		read(vmi_sock,buf,100);
+		if(!strcmp(buf,"paused\n\r")) {
+			vm->paused=1;
+			free(vm->attacker);
+			vm->attacker=NULL;
+		} else {
+			printf("This attacker is already using a VM: %u\n", *vmID);
+			search->vmID=*vmID;
+			return TRUE;
+		}
 	}
 
 	return FALSE;
@@ -257,7 +271,8 @@ void mod_vmi_pick(struct mod_args *args)
 	g_printerr("%s VMI Backpick Module called\n", H(args->pkt->conn->id));
 
 	struct vm_search search;
-        search.srcIP = *(g_strsplit( args->pkt->key_src, ":", 0));
+	gchar **values=g_strsplit( args->pkt->key_src, ":", 0);
+        search.srcIP = values[0];
 	search.vmID=0;
 
 	g_tree_foreach(vmi_vms, (GTraverseFunc)find_used_vm, (gpointer)&search);
@@ -275,34 +290,135 @@ void mod_vmi_pick(struct mod_args *args)
 		args->node->result = 0;
 	}
 
-	free(search.srcIP);
+	g_strfreev(values);
 }
 
 void mod_vmi_back(struct mod_args *args)
 {
-
 	g_printerr("%s VMI Back Module called\n", H(args->pkt->conn->id));
 
+	GTimeVal t;
+        g_get_current_time(&t);
+        gint now = (t.tv_sec);
+
+	// Check if IP is banned (exceeded allowed time-frame)
+	gchar **key_src;
+        key_src = g_strsplit( args->pkt->key_src, ":", 0);
+	gint *start=(gint *)g_tree_lookup(bannedIPs, key_src[0]);
+	g_strfreev(key_src);
+
+	if(start!=NULL && now-(*start)>ATTACK_TIMEOUT) {
+		args->node->result = 0;
+		return;
+	}
+
+	int n=write(vmi_sock, "free\n", strlen("free\n"));
+
+	if(n<=0) { args->node->result = 0; return; }
+
+	char buf[100];
+	bzero(buf,100);
+	n=read(vmi_sock,buf,100);
+
+	if(n<=0) { args->node->result = 0; return; }
+
+	char *nl = strrchr(buf, '\r');
+        if (nl) *nl = '\0';
+        nl = strrchr(buf, '\n');
+        if (nl) *nl = '\0';
+
+	int free=atoi(buf);
+
+	if(free>0) {
+		args->node->result = 1;
+
+		key_src = g_strsplit( args->pkt->key_src, ":", 0);
+		char *attacker=strdup(key_src[0]);
+		g_strfreev(key_src);
+		gint *startTime=malloc(sizeof(gint));
+		*startTime=now;
+
+		g_tree_insert(bannedIPs, (gpointer)attacker, (gpointer)startTime);
+	}
+	else		args->node->result = 0;
+
+}
+
+gboolean control_check_attacker(gpointer key, gpointer value, gpointer data) {
+	struct vmi_vm *vm=(struct vmi_vm *)value;
+	struct attacker_search *search=(struct attacker_search *)data;
+	uint32_t vmID=*(uint32_t *)key;
+
+	GTimeVal t;
+        g_get_current_time(&t);
+        gint now = (t.tv_sec);
+
+	if(!vm->paused && vmID==search->mark && now-(vm->start)<=ATTACK_TIMEOUT) {
+		search->found=1;
+		search->vmName=strdup(vm->name);
+		search->attacker=strdup(vm->attacker);
+
+		if(vm->event)
+			search->event=1;
+		else
+		// Is the clone going somewhere else than the attacker?
+		if(strcmp(search->attacker, vm->attacker)) {
+			search->event=1;
+			vm->event=1;
+		}
+
+		return TRUE;
+	} else
+	return FALSE;
 }
 
 void mod_vmi_control(struct mod_args *args)
 {
+	// Only control packets coming from the clones
+	if(args->pkt->mark==0) {
+		args->node->result = 1;
+		return;
+	}
 
 	g_printerr("%s VMI Control Module called\n", H(args->pkt->conn->id));
 
-	//int expiration = 24*3600; /* a day */
-
-	/*GTimeVal t;
-        g_get_current_time(&t);
-        gint now = (t.tv_sec);*/
-
 	/*! get the IP address from the packet */
-	gchar **key_src,**key_dst;
-	key_src = g_strsplit( args->pkt->key_src, ":", 0);
+	gchar **key_dst;
+	//key_src = g_strsplit( args->pkt->key_src, ":", 0);
 	key_dst =  g_strsplit(args->pkt->key_dst, ":", 2);
 
 	//g_printerr("%s source IP is %s\n", H(args->pkt->conn->id), key_src[0]);
 
+	struct attacker_search search;
+	search.found=0;
+	search.event=0;
+	search.outIP=key_dst[0];
+	search.attacker=NULL;
+	search.mark=args->pkt->mark;
+	search.vmName=NULL;
+
+	g_tree_foreach(vmi_vms, (GTraverseFunc)control_check_attacker, (gpointer)&search);
+
+	if(search.found && search.vmName!=NULL) {
+		if(search.event) {
+			g_printerr("%s Cought network event, sending signal!\n", H(args->pkt->conn->id));
+			args->node->result = 0;
+
+			char *buf = g_malloc0(snprintf(NULL, 0, "%s,%s,%s\n", search.vmName, search.attacker, search.outIP) + 1);
+                	sprintf(buf,"%s,%s,%s\n",search.vmName,search.attacker,search.outIP);
+                	write(vmi_sock, buf, strlen(buf));
+			free(buf);
+		}
+		else
+			args->node->result = 1;
+
+		free(search.vmName);
+		free(search.attacker);
+	} else {
+		args->node->result = 0;
+	}
+
+	g_strfreev(key_dst);
 	return;
 }
 
@@ -375,6 +491,8 @@ int init_mod_vmi() {
 
 	vmi_vms=g_tree_new_full((GCompareDataFunc)intcmp, NULL, (GDestroyNotify)noop, (GDestroyNotify)free_vmi_vm);
         g_ptr_array_foreach(targets, (GFunc)build_vmi_vms, (gpointer)&vmi_sock);
+
+	bannedIPs=g_tree_new_full((GCompareDataFunc)strcmp, NULL, (GDestroyNotify)free, (GDestroyNotify)free);
 
 	return 0;
 }
